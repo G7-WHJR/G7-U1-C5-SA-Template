639da8074356d90df1a922e7884f6de06d228e98
unknown <raveen.anand@whitehatjr.com>
2021-10-21 19:46:57 +0530

    v2

---
+++ Form Validation/myscript.js
@@ -0,0 +1,29 @@
+function validate()
+{
+    
+    var name = document.getElementById("uname").value;
+
+   //1. Take input from contact field(id :"contact")
+    var phone = document.getElementById("").value;
+
+    //2. calculate length of contact number (stored in variable "phone")
+    var count= .length;
+    
+    if(name == ''){
+        document.getElementById("err1").style.display="block";
+    }
+    else{
+        document.getElementById("err1").style.display="none";
+    }
+
+    //3. if count is not equal to 10, then display the 'err2' element by setting the display property to "block"
+    //else hide the 'err2' element by setting the display property to "none"
+    if(){
+        document.getElementById("").style.display="";
+    }
+    else{
+        document.getElementById("").style.display="";
+    }
+    
+}
+
